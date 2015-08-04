312d41ab665681cf074b4113eb443e17716d1bec
mmarshall-rakuten <michael.marshall@rakuten.com>
2015-08-04 10:39:46 +0100

    Re-built the test project.

--- src/Rakuten.Catalog.AspNet.Mvc.Tests/Properties/AssemblyInfo.cs
+++ src/Rakuten.Catalog.AspNet.Mvc.Tests/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rakuten.Catalog.AspNet.Mvc.Tests")]
@@ -14,22 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bed9db07-7d09-4fb4-a24d-c81da7777789")]
+[assembly: Guid("cca877c8-36c6-4f93-848a-4cce08da1d77")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Rakuten.Catalog.AspNet.Mvc.Tests/Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Rakuten.Catalog.AspNet.Mvc.Tests.Tests.Controllers
+{
+    using System.Web.Mvc;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Rakuten.Catalog.AspNet.Mvc.Controllers;
+
+    /// <summary>
+    /// The suite of tests for the <see cref="HomeController"/> class.
+    /// </summary>
+    [TestClass]
+    public class HomeControllerTests
+    {
+        /// <summary>
+        /// Ensures that a non null result is returned when the Index method is invoked.
+        /// </summary>
+        [TestMethod]
+        public void HomeControllerIndexReturnsANonNullView()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var view = controller.Index();
+
+            // Assert
+            Assert.IsNotNull(view);
+        }
+
+        /// <summary>
+        /// Ensures that a <see cref="ViewResult"/> instance is returned from the Index method.
+        /// </summary>
+        [TestMethod]
+        public void HomeControllerIndexReturnsAViewResult()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var view = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(view, typeof(ViewResult));
+        }
+    }
+}
