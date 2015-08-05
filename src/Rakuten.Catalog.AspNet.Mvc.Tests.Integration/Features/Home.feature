69a3ae9dcad65a9e33922e1ae69ad92b46216e4f
mmarshall-rakuten <michael.marshall@rakuten.com>
2015-08-05 15:12:21 +0100

    Added a single integration test.

---
+++ src/Rakuten.Catalog.AspNet.Mvc.Tests.Integration/Features/Home.feature
@@ -0,0 +1,5 @@
+﻿Feature: Home
+
+Scenario: The version number should be displayed in the footer
+	Given I have navigated to the website
+	Then I want to see the version number
---
+++ src/Rakuten.Catalog.AspNet.Mvc.Tests.Integration/Steps/Home.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Rakuten.Catalog.AspNet.Mvc.Tests.Integration
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+
+    using TechTalk.SpecFlow;    
+    
+    [Binding]
+    public class Home
+    {
+        private const string ScenarioContextKey = "IWebDriver";
+
+        [Given(@"I have navigated to the website")]
+        public void GivenIHaveNavigatedToTheWebsite()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--log-level=3");
+            chromeOptions.AddArgument("--lang=en-US");
+
+            IWebDriver webDriver = new ChromeDriver(chromeOptions);
+
+            webDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            webDriver.Navigate().GoToUrl("http://191.235.185.238/");
+
+            ScenarioContext.Current.Add(ScenarioContextKey, webDriver);
+        }
+
+        [Then(@"I want to see the version number")]
+        public void ThenIWantToSeeTheVersionNumber()
+        {
+            var webDriver = ScenarioContext.Current.Get<IWebDriver>(ScenarioContextKey);
+
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+            IWebElement footer = wait.Until(
+                ExpectedConditions.ElementIsVisible(By.CssSelector("footer > p")));
+
+            Assert.IsNotNull(footer.Text);
+
+            webDriver.Dispose();
+        }
+
+    }
+}
