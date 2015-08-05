6468ba3a16892afe83d6f3ec33ec0c4df954c1b8
mmarshall-rakuten <michael.marshall@rakuten.com>
2015-08-05 10:49:35 +0100

    Moved the using statements in each cs file.

--- src/Rakuten.Catalog.AspNet.Mvc/Controllers/HomeController.cs
+++ src/Rakuten.Catalog.AspNet.Mvc/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
+﻿
 namespace Rakuten.Catalog.AspNet.Mvc.Controllers
 {
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
